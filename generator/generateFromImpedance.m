f15fbb68dc9a255a0d29d5af094daffb7b1a56b0
vittoriom94 <vimattei@hotmail.it>
2019-10-12 11:52:39 +0200

    Added impedance generation from existing impedance function

--- generator/generateFromImpedance.m
+++ generator/generateFromImpedance.m
@@ -1,17 +1,34 @@
-function [outputArg1,outputArg2] = generateFromImpedance(Z,amount)
+function newZ = generateFromImpedance(Z,amount)
 
 Zr = real(Z);
 Zi = imag(Z);
-Ztot = cell(amount,1);
 
-weights = [zeros(1,12) linspace(0,1,36)];
-noise = weights'.*0.05.*(rand(48,2)-0.5);
-
-data = (Zr+Zr.*noise(:,1))+1i*(Zi+Zi.*noise(:,2));
-
+figure()
+hold on
+% plot(Zr,-Zi,'-+r');
+newZ = cell(amount,1);
 for i=1:amount
     
+    weights = [0.35*rand(1,12) (0.5+0.5*rand(1,36)).*linspace(0.1,1,36)];
+    noise = weights'.*0.075.*(rand(48,2)-0.5);
+
+    scalingFactor =  0.5*rand()+0.75; %scalare
+    costantAddReal = max(abs(Zr))*0.2*rand()-0.1; %costante per parte reale
+    costantAddImag = max(abs(Zi))*0.2*rand()-0.1; %costante per parte imag
+    rotateFactor  = 10*rand()-5;% gradi 
+    data = scalingFactor*Z;
+    data = (real(data)+real(data).*noise(:,1))+1i*(imag(data)+imag(data).*noise(:,2));
     
+
+    % questo non dovrebbe servirmi perchè poi il riporto nell'origine
+%     data = costantAddReal+real(data)+1i*(costantAddImag+imag(data));
+    noShow = figure('Visible',false);
+    plotObject = plot(real(data),-imag(data));
+    rotate(plotObject,[0 0 1], rotateFactor);
+    data = get(plotObject,'XData') -1i*get(plotObject,'YData'); 
+    close(noShow);
+%     plot(real(data),-imag(data),'-k')
+    newZ{i} = data';
 end
 
 
@@ -20,7 +37,7 @@ end
 
 % rumore lineare
 % rumore pesato
-% rumore awgn
 % moltiplica tutto per un fattore
+% aggiungi una costante
 % ruota rotate(h,[0 0 1],1), h è la linea fatta con plot
 % poi faccio x=get(h,'Xdata') y=get(h,'Ydata')
